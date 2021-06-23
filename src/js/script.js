c404e5312c65ff0866e1072e9a89e3491034a296
ScionRs <grazs116@gmail.com>
2021-06-23 12:21:20 +0300

    Added about-section

--- src/js/script.js
+++ src/js/script.js
@@ -14,4 +14,36 @@ $(document).ready(function() {
             menu.classList.toggle('header__menu-active');
         })
     })
+
+    const tabs = document.querySelectorAll('.about__tab'),
+        tabsContent = document.querySelectorAll('.about__items'),
+        tabsParent = document.querySelector('.about__tabs');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+        tabs.forEach(item => {
+            item.classList.remove('about__tab-active');
+        });
+    }
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'flex';
+        tabs[i].classList.add('about__tab-active');
+    }
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click',(event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('about__tab')){
+            tabs.forEach((item,i) =>{
+                if (target == item){
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 })
